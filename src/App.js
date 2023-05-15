38a42277d3e50cac601c6bbd3aba769735310b6e
Andrej Fidel <andrej@hudlajf.com>
2023-05-15 22:28:50 +0200

    feat: Add basic card styling

--- src/App.js
+++ src/App.js
@@ -3,57 +3,67 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 var countryCodes = [
-  'US', // United States
-  'CA', // Canada
-  'MX', // Mexico
-  'GB', // United Kingdom
-  'FR', // France
-  'DE', // Germany
-  'IT', // Italy
-  'ES', // Spain
-  'NL', // Netherlands
-  'SE', // Sweden
-  'NO', // Norway
-  'FI', // Finland
-  'DK', // Denmark
-  'RU', // Russia
-  'CN', // China
-  'JP', // Japan
-  'KR', // South Korea
-  'IN', // India
-  'PK', // Pakistan
-  'BD', // Bangladesh
-  'ID', // Indonesia
-  'PH', // Philippines
-  'VN', // Vietnam
-  'BR', // Brazil
-  'AR', // Argentina
-  'CO', // Colombia
-  'PE', // Peru
-  'ZA', // South Africa
-  'NG', // Nigeria
-  'EG', // Egypt
-  'SA', // Saudi Arabia
-  'AE', // United Arab Emirates
-  'IL', // Israel
-  'TR', // Turkey
-  'GR', // Greece
-  'PT', // Portugal
-  'AU', // Australia
-  'NZ', // New Zealand
-  'SG', // Singapore
-  'MY', // Malaysia
-  'TH', // Thailand
-  'KH', // Cambodia
-  'KE', // Kenya
-  'ET', // Ethiopia
-  'GH', // Ghana
-  'UA', // Ukraine
-  'PL', // Poland
-  'AT', // Austria
-  'CH', // Switzerland
-  'BE', // Belgium
+  'us', // United States
+  'ca', // Canada
+  'mx', // Mexico
+  'gb', // United Kingdom
+  'fr', // France
+  'de', // Germany
+  'it', // Italy
+  'es', // Spain
+  'nl', // Netherlands
+  'se', // Sweden
+  'no', // Norway
+  'fi', // Finland
+  'dk', // Denmark
+  'ru', // Russia
+  'cn', // China
+  'jp', // Japan
+  'kr', // South Korea
+  'in', // India
+  'pk', // Pakistan
+  'bd', // Bangladesh
+  'id', // Indonesia
+  'ph', // Philippines
+  'vn', // Vietnam
+  'br', // Brazil
+  'ar', // Argentina
+  'co', // Colombia
+  'pe', // Peru
+  'za', // South Africa
+  'ng', // Nigeria
+  'eg', // Egypt
+  'sa', // Saudi Arabia
+  'ae', // United Arab Emirates
+  'il', // Israel
+  'tr', // Turkey
+  'gr', // Greece
+  'pt', // Portugal
+  'au', // Australia
+  'nz', // New Zealand
+  'sg', // Singapore
+  'my', // Malaysia
+  'th', // Thailand
+  'kh', // Cambodia
+  'ke', // Kenya
+  'et', // Ethiopia
+  'gh', // Ghana
+  'ua', // Ukraine
+  'pl', // Poland
+  'at', // Austria
+  'ch', // Switzerland
+  'be', // Belgium
 ];
+
+function getMatchStatus(match) {
+  if (match.status._id === 100) {
+    return 'postmatch'
+  } else if (match.status._id === 0) {
+    return 'prematch'
+  } else {
+    return 'live'
+  }
+}
 
 function App() {
   const [sports, setSports] = useState([])
@@ -87,18 +97,40 @@ function App() {
                     <h3>{tournament.name}</h3>
                     <strong>{tournament.seasontypename}</strong>
                     {tournament.matches.map(match => (
-                      <>
-                        <p>{ match.status.name }</p>
-                        <p>{ match.status._id }</p>
-                        <p>{ match.teams.home.name }</p>
-                        <p>{ match.teams.home.abbr }</p>
-                        <p>{ match.teams.home.uid }</p>
-                        <img src={`https://flagsapi.com/${countryCodes[match.teams.home.uid%50]}/flat/64.png`} alt={match.teams.home.name}></img>
-                        <p>{ match.teams.away.name }</p>
-                        <p>{ match.teams.away.abbr }</p>
-                        <p>{ match.teams.away.uid }</p>
-                        <img src={`https://flagsapi.com/${countryCodes[match.teams.away.uid%50]}/flat/64.png`} alt={match.teams.away.name}></img>
-                      </>
+                      <div className={ `card card--status-${getMatchStatus(match)}` }>
+                        <span className="card__titles-container">
+                          <span className="card__title">{tournament.name}</span>
+                          <span className="card__subtitle">{tournament.seasontypename}</span>
+                        </span>
+                        <div className="card__center-container">
+                          <div className="card__country">
+                            <img className="card__country-flag" src={`https://flagcdn.com/w160/${countryCodes[match.teams.home.uid%50]}.webp`} alt={match.teams.home.name} />
+                            <span className="card__country-name">{ match.teams.home.name }</span>
+                          </div>
+                          {
+                            getMatchStatus(match) === 'prematch' ?
+                              (
+                                <div className="card__vs card__vs--prematch">
+                                  <span className="card__vs-title">VS</span>
+                                  <span className="card__vs-time">{match._dt.time}</span>
+                                  <span className="card__vs-date">{match._dt.date}</span>
+                                </div>
+                              ) :
+                              (
+                                <div className={ `card__vs card__vs--${getMatchStatus(match)}` }>
+                                  <span>{match.result.home}</span>
+                                  <span className="card__vs-separator">:</span>
+                                  <span>{match.result.away}</span>
+                                </div>
+                              )
+                          }
+                          <div className="card__country">
+                            <img className="card__country-flag" src={`https://flagcdn.com/w160/${countryCodes[match.teams.away.uid%50]}.webp`} alt={match.teams.away.name}></img>
+                            <span className="card__country-name">{ match.teams.away.name }</span>
+                          </div>
+                        </div>
+                        <p className={ `card__status card__status--${getMatchStatus(match)}`}>{ match.status.name }</p>
+                      </div>
                     ))}
                   </>
                 ))}
