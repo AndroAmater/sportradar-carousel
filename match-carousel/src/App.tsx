d9f385bab97155702e310548dc7192113406a3b3
Andrej Fidel <andrej@hudlajf.com>
2023-05-24 01:50:08 +0200

    feat: Add tabs

--- match-carousel/src/App.tsx
+++ match-carousel/src/App.tsx
@@ -3,19 +3,45 @@ import './App.css';
 import MatchCarousel from './components/MatchCarousel';
 
 function App() {
-  return (
-    <div className="App">
+  const [activeTab, setActiveTab] = React.useState(1)
+
+  let content
+
+  if (activeTab === 1) {
+    content = (
       <MatchCarousel 
         max={15}
         logRenderCount={true}
       />
-      <MatchCarousel
-        sportId={1}
-      />
-      <MatchCarousel
-        sportId={2}
-      />
+    )
+  } else {
+    content = (
+      <>
+        <MatchCarousel
+          sportId={1}
+        />
+        <MatchCarousel
+          sportId={2}
+        />
+      </>
+    )
+  }
+
+  return (
+    <div className="App">
+      <div className="tab-container">
+        <button 
+          className={ `tab ${activeTab === 1 ? 'tab--active' : ''}` }
+          onClick={() => setActiveTab(1)}
+        >Tab 1</button>
+        <button 
+          className={ `tab ${activeTab === 2 ? 'tab--active' : ''}` }
+          onClick={() => setActiveTab(2)}
+        >Tab 2</button>
+      </div>
+      {content}
     </div>
   );
 }
+
 export default App;
